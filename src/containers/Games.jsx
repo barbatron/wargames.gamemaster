b393d0420b6c5a64cffad447fe927828af1dae5f
Joakim Olsson <jolsson@spotify.com>
2018-04-10 18:09:25 +0200

    Grid it!

--- src/containers/Games.jsx
+++ src/containers/Games.jsx
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 import {requestGames} from '../actions/game';
+import {withStyles} from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
 class Games extends Component {
   componentDidMount() {
@@ -8,20 +24,32 @@ class Games extends Component {
   }
 
   render() {
+    const {classes} = this.props;
     const {isFetchingGames, games} = this.props;
     const gameItems = (games || []).map(game => (<li key={game.id}>{game.name}</li>));
     return (
-      <div>
-        <h4>isFetching?</h4>
-        {isFetchingGames}
-        <h4>Games</h4>
-        <ul>
-          {gameItems}
-        </ul>
-      </div>
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={12} sm={2}>
+          <div>
+            <h4>isFetching?</h4>
+            {isFetchingGames}
+            <h4>Games</h4>
+            <ul>
+              {gameItems}
+            </ul>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={10}>
+          Hello!
+        </Grid>
+      </Grid>
     );
   }
 }
+
+Games.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 function mapStateToProps(state) {
   const {isFetchingGames, games} = state.gamesList;
@@ -31,4 +59,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(Games);
+export default withStyles(styles)(connect(mapStateToProps)(Games));
